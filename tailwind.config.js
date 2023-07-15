cfa0f3040f1a6e1aa3d0d226ba9d5ce5f3adf7bb
ARZIELLE ARCEGA <jodespanola@gmail.com>
2023-07-15 12:08:39 +0800

    solution for qr code component

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
+    colors: {
+      white : 'hsl(0, 0%, 100%)',
+      light_gray : 'hsl(212, 45%, 89%)',
+      grayish_blue : 'hsl(220, 15%, 55%)',
+      dark_blue : 'hsl(218, 44%, 22%)'
+    }
   },
   plugins: [],
 }
